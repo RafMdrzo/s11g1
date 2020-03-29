4deccd4700a4d41a8158496d41c4324a2ccdc1d6
Eugenio Pastoral <stonenobrein@gmail.com>
2020-03-29 19:22:11 +0800

    Post data (including comments) are now hardcoded

--- routes/routes.js
+++ routes/routes.js
@@ -27,14 +27,88 @@ app.get("/", function(req, res)
           post_title: 'Manila Bay',
           post_author: 'Eugenio Pastoral',
           post_elapsed: '8 hours ago',
-          post_description: 'I teased this shoot over on my Instagram a few weeks ago. Taken two days before Christmas last year. I first attempted to take a shot of this way back in November 2018 but was unsuccessful to my eyes. Fortunately, I spent that night near the location with all my gear.'
+          post_description: 'I teased this shoot over on my Instagram a few weeks ago. Taken two days before Christmas last year. I first attempted to take a shot of this way back in November 2018 but was unsuccessful to my eyes. Fortunately, I spent that night near the location with all my gear.',
+          post_id: 'com1',
+          comment: [
+            {
+              profpic: 'img/le.jpg',
+              name: 'Leanne Loyola',
+              text: 'Woah! Amazing 🌝'
+            },
+
+            {
+              profpic: 'img/thea.jpg',
+              name: 'Thea Go',
+              text: 'corpse'
+            },
+
+            {
+              profpic: 'img/boss.jpg',
+              name: 'Alexis Dela Cruz',
+              text: 'Awitt'
+            },
+            {
+              profpic: 'img/albert.jpg',
+              name: 'Albert Bofill',
+              text: 'Nice one'
+            },
+
+            {
+              profpic: 'img/mica.jpg',
+              name: 'Michaela Dizon',
+              text: 'haha'
+            },
+
+            {
+              profpic: 'img/raf.jpg',
+              name: 'Rafael Maderazo',
+              text: 'Nagtataka padin ako kung bakit ka single'
+            }
+          ]
         },
         {
           post_image: 'img/landing4.jpg',
           post_title: 'Lights',
           post_author: 'Eugenio Pastoral',
           post_elapsed: '12 hours ago',
-          post_description: 'It was a day before Christmas Eve. It has been raining continuously for a few days. Overlooking the streets, I was amused by how passing cars light up the moist window.'
+          post_description: 'It was a day before Christmas Eve. It has been raining continuously for a few days. Overlooking the streets, I was amused by how passing cars light up the moist window.',
+          post_id: 'com2',
+          comment: [
+            {
+              profpic: 'img/le.jpg',
+              name: 'Leanne Loyola',
+              text: 'Gandaaa'
+            },
+
+            {
+              profpic: 'img/thea.jpg',
+              name: 'Thea Go',
+              text: 'Mama mo padin'
+            },
+
+            {
+              profpic: 'img/boss.jpg',
+              name: 'Alexis Dela Cruz',
+              text: 'XDDD'
+            },
+            {
+              profpic: 'img/albert.jpg',
+              name: 'Albert Bofill',
+              text: 'Grabe'
+            },
+
+            {
+              profpic: 'img/mica.jpg',
+              name: 'Michaela Dizon',
+              text: 'lmao'
+            },
+
+            {
+              profpic: 'img/raf.jpg',
+              name: 'Rafael Maderazo',
+              text: 'Single po siya'
+            }
+          ]
         }
       ]
     });
@@ -83,12 +157,12 @@ response.end();
 
 //logout
 app.get('/logout', function(req, res) {
-    req.session.destroy((err) => {
-        if(err) {
-            return console.log(err);
-        }
-        res.redirect('/');
-    });
+  req.session.destroy((err) => {
+    if(err) {
+      return console.log(err);
+    }
+    res.redirect('/');
+  });
 });
 
 app.get("/user/:username", function(req, res)
