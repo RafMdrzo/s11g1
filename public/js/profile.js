22cc921885ce66ce4f2da93f70f5934a878be756
Rafael Maderazo <rafaelmaderazo@gmail.com>
2020-04-02 15:34:09 +0800

    removed hardcode style of updating in profile.js

--- public/js/profile.js
+++ public/js/profile.js
@@ -11,59 +11,16 @@ $(document).ready(function()
         window.location.href = "logout";
     });
 
-    function readURL(input)
+
+    $("#navfolio #home-red").click(function()
     {
-        if (input.files && input.files[0])
-        {
-          var reader = new FileReader();
-
-          reader.onload = function(e)
-          {
-            $('#display-pic').attr('src', e.target.result);
-          }
-
-          reader.readAsDataURL(input.files[0]);
-        }
-    }
-
-    $("#imgInp").change(function()
-    {
-      readURL(this);
+        window.location.href = "home";
     });
 
+    $(".gallery-column a").click(function()
+    {
+      var imgLoc = $(this).children("div").children("img").attr("src");
+      $("#view-pic-cont").attr("src", imgLoc);
 
-    $("#newhandle").val($(".profile-handle").text());
-    $("#newloc").val($(".user-location").text());
-    $("#newbio").val($(".user-desc").text());
-    $("#display-pic").attr('src', $(".prof-img-cont img").attr("src"));
-
-
-      $("#save-dat").click(function()
-      {
-        var handle = $("#newhandle").val();
-        var bio = $("#newbio").val();
-        var loc = $("#newloc").val();
-
-        $("#handle").text(handle);
-        $("#bio").text(bio);
-        $("#loc").text(loc);
-
-
-        $(".prof-img-cont img").attr("src", $("#display-pic").attr('src'));
-        $(".nav-avatar").attr("src", $("#display-pic").attr('src'));
-
-      });
-
-
-      $("#navfolio #home-red").click(function()
-      {
-          window.location.href = "home";
-      });
-
-      $(".gallery-column a").click(function()
-      {
-        var imgLoc = $(this).children("div").children("img").attr("src");
-        $("#view-pic-cont").attr("src", imgLoc);
-
-      });
+    });
 });
