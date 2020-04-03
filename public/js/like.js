574bb6b7e78c807bb85484bc575e760cae6f3c21
Eugenio Pastoral <stonenobrein@gmail.com>
2020-04-03 21:33:46 +0800

    Unlike functionality added

--- public/js/like.js
+++ public/js/like.js
@@ -1,13 +1,24 @@
 $(document).ready(function() {
   $('#unliked').click(function() {
     var post_id = ($(this).attr("class")).substring(1);
-    alert(post_id);
     $.post('/likepost', {post_id: post_id}, function(result) {
       if(result != null) {
-        alert(result);
         $('#unlikedpic').attr("src", "img/heart2.png");
         $('#unlikedpic').attr("id", "likedpic");
         $('#unliked').attr("id", "liked");
+      }
+      else
+        return;
+    });
+  });
+
+  $('#liked').click(function() {
+    var post_id = ($(this).attr("class")).substring(1);
+    $.post('/unlikepost', {post_id: post_id}, function(result) {
+      if(result != null) {
+        $('#likedpic').attr("src", "img/heart.png");
+        $('#likedpic').attr("id", "unlikedpic");
+        $('#liked').attr("id", "unliked");
       }
       else
         return;
